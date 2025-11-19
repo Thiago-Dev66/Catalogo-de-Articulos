6799465d0b2a1ba709de594babe15c1493139ccc
Santiago Figueredo <figueredosantiago2000@gmail.com>
2025-11-18 23:39:42 -0300

    First commit

---
+++ TPFinalNivel2_Figueredo/ClassLibrary1/Articulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int IdMarca { get; set; }
+        public int IdCategoria { get; set; }
+        public int UrlImagen { get; set; }
+        public int Precio { get; set; }
+    }
+}
